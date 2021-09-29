23d678c27c10dba7b64182dd718ee12cbb17a98b
fustinose <fustinosej@gmail.com>
2021-09-29 17:28:25 -0400

    attempt at assigning reviewer with bulk action

--- mayan/apps/documents/workflow_actions.py
+++ mayan/apps/documents/workflow_actions.py
@@ -33,6 +33,33 @@ class DocumentTypeChangeAction(WorkflowAction):
         return DocumentType.objects.get(pk=self.form_data.get('document_type'))
 
 
+class DocumentReviewerAssignAction(WorkflowAction):
+    fields = {
+        'document_type': {
+            'label': _('Document type'),
+            'class': 'django.forms.ModelChoiceField', 'kwargs': {
+                'help_text': _('New document type for the workflow document.'),
+                'queryset': DocumentType.objects.all(), 'required': True
+            }
+        }
+    }
+    label = _('Assign Reviewer')
+    widgets = {
+        'workflows': {
+            'class': 'django.forms.widgets.Select', 'kwargs': {
+                'attrs': {'class': 'select2'},
+            }
+        }
+    }
+
+    def execute(self, context):
+        context['document'].document_type_change(
+            document_type=self.get_document_type()
+        )
+
+    def get_document_type(self):
+        return DocumentType.objects.get(pk=self.form_data.get('document_type'))
+
 class TrashDocumentAction(WorkflowAction):
     label = _('Send document to trash')
 
